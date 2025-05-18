72a599feb92ba3e256eb1d8dd1538a4fe3e6ef7f
evanliu23 <eliu23@bu.edu>
2025-05-18 13:50:37 -0400

    Getting started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to my journey, stay tuned for more!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>The Evan Liu Journey</h1>
+      <nav>
+        <a href="#about">About</a> | <a href="#resume">Resume</a> | <a href="#projects">Projects</a> | <a href="#contact">Let's connect!</a> 
+      </nav>
+    </header>
+  );
+}
+
+export default Header; 
