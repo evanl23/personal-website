fa1fd6924bbf8adbb6bd90c794b18c913a7b6246
evanliu23 <eliu23@bu.edu>
2025-05-28 15:36:19 -0400

    Add mobile nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {FaTimes, FaBars} from 'react-icons/fa'
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <nav className="container flex justify-between items-center mx-auto p-6 px-4">
+    <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white ">
+      <nav className="container flex justify-between items-center mx-auto p-6 px-4 ">
         <li className="list-none"> {/*"font-bold text-lg cursor-pointer"*/}
           <Link href="/">
             <span className="font-black text-2xl flex items-center">
@@ -18,8 +21,9 @@ function Header() {
             </span>
           </Link>
         </li>
-
-        <div className="font-black text-l flex gap-7 ">
+        
+        {/* Desktop navbar */}
+        <div className="font-black text-l flex gap-7 hidden md:flex">
           < Link to="/" className="dark:text-gray-400 dark:hover:text-white transition-colors hover:rotate-360 transition-transform duration-500">
             Home
           </Link>
@@ -33,6 +37,30 @@ function Header() {
             Connect
           </Link>
         </div>
+
+        {/* Mobile navbar */}
+        <div className="md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
+              {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+        </div>
+
+        {isOpen && //Check if mobile menue is open
+          <div className="md:hidden font-black text-l flex gap-7 ">
+            < Link to="/" className="dark:text-gray-400 dark:hover:text-white transition-colors hover:rotate-360 transition-transform duration-500" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
+            <Link to="/projects" className="dark:text-gray-400 dark:hover:text-white transition-colors hover:rotate-360 transition-transform duration-500" onClick={() => setIsOpen(false)}>
+              Projects
+            </Link>
+            <Link to="/resume" className=" dark:text-gray-400 dark:hover:text-white transition-colors hover:rotate-360 transition-transform duration-500" onClick={() => setIsOpen(false)}>
+              Resume
+            </Link>
+              <Link to="/connect" className="dark:text-gray-400 dark:hover:text-white transition-colors hover:rotate-360 transition-transform duration-500" onClick={() => setIsOpen(false)}>
+              Connect
+            </Link>
+          </div>
+        }
       </nav>
     </header>
   );
